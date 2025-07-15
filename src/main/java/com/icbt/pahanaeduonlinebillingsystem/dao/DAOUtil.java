4f51684f45a0f82704eb69a5bb1d9b18da96a6f3
thisarakavishka <thisarakavishka0@gmail.com>
2025-07-16 02:28:28 +0530

    feat: add DAOUtil for executing parameterized SQL queries

---
+++ src/main/java/com/icbt/pahanaeduonlinebillingsystem/dao/DAOUtil.java
@@ -0,0 +1,29 @@
+package com.icbt.pahanaeduonlinebillingsystem.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * @author Thisara Kavishka
+ * @date 2025-07-14
+ * @since 1.0
+ */
+public class DAOUtil {
+
+    public static <T> T executeSql(Connection connection, String sql, Object... params) throws SQLException{
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            for (int i = 0; i < params.length; i++) {
+                preparedStatement.setObject((i + 1), params[i]);
+            }
+
+            if (sql.trim().toUpperCase().startsWith("SELECT")) {
+                return (T) preparedStatement.executeQuery();
+            }
+
+            return (T) ((Boolean) (preparedStatement.executeUpdate() > 0));
+        }
+    }
+}
